e3937bc8f214480b214a383a2d80ce52fb5dca43
ChairmanWangII <50027145+ChairmanWangII@users.noreply.github.com>
2022-10-05 02:20:24 +0800

    Feat:add orthogonal linked list into our matrix family

---
+++ OrthogonalLinkedList.go
@@ -0,0 +1,21 @@
+package gooseberrymat
+
+type OrthogonalLinkedList struct {
+	Width   int
+	Height  int
+	NotNull int
+	Col     []*OrthogonalLinkedNode
+	Row     []*OrthogonalLinkedNode
+}
+
+type OrthogonalLinkedNode struct {
+	Col       int
+	Row       int
+	Val       int
+	DownNode  *OrthogonalLinkedNode
+	RightNode *OrthogonalLinkedNode
+}
+
+func (ol *OrthogonalLinkedList) Constructor(matrix *Grid) *OrthogonalLinkedList {
+	return nil
+}
